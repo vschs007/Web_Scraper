9bb8767e6efb436c5bf40bc8dc6836e9d430bc6d
Vivek Kumar Singh <vschs007@gmail.com>
2018-01-26 16:27:18 +0530

    updated path
    >comments
    >path updation

--- imdb.py
+++ imdb.py
@@ -1,8 +1,9 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
-#driver=webdriver.Chrome(executable_path=r'F:\chromedriver_win32\chromedriver.exe')
+#driver=webdriver.Chrome(executable_path=r'F:\chromedriver_win32\chromedriver.exe') ------ use this for chromedriver
 driver=webdriver.PhantomJS(executable_path=r'F:\phantomjs-2.1.1-windows\phantomjs-2.1.1-windows\bin\phantomjs.exe')
 
+#you need to give the path of your phantomjs in line 4 , instead of my path.
 
 driver.get('http://www.imdb.com/chart/top?ref_=nv_mv_250_6')
 html_doc=driver.page_source
@@ -11,6 +12,8 @@ soup=BeautifulSoup(html_doc,'lxml')
 table=soup.find('table',class_='chart')
 for td in table.find_all('td',class_='titleColumn'):
 	print(td.text.strip().replace('\n','').replace('      ',' '))
+	
+#line 14 indents correctly the output.
 
 
 driver.quit()
